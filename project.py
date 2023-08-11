c5e4d2810e60d25c43f7c85e6917ddfa9e687c89
Jimmy Vo <j.vo718@gmail.com>
2023-08-11 08:58:42 -0400

    add test file

--- project.py
+++ project.py
@@ -1,15 +1,28 @@
+import requests
 
 def main():
-    ...
+    parse_json()
 
-def func1():
-    ...
+def parse_json():
+    json_arr = get_api() 
+    # for obj in json_arr:
+    #     sentiment = {obj['sentiment']}
+    #     ticker = {obj['ticker']}
+    #     n_comments = {obj['no_of_comments']}
+    #     print(f"The boys are {sentiment} about {ticker}")
+    print(json_arr)
 
-def func2():
-    ...
 
-def func3():
-    ...
+
+def get_api():
+    url = "https://tradestie.com/api/v1/apps/reddit" 
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
 
 if __name__ == "__main__":
-    main():
+    main()
---
+++ test_project.py
@@ -0,0 +1,10 @@
+from project import func1, func2, func3
+
+def test_func1():
+    assert func1() == "test"
+
+def test_func2():
+    assert func2() == "test"
+
+def test_func3():
+    assert func3() == "test"
